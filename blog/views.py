c3357392f3f1db607d50302d74d9faf3b950f4bf
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-02-09 16:16:42 -0200

    Adicionando tags e posts similares

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
+from django.db.models import Count
 
 
 
@@ -15,24 +17,34 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-# def post_list(request):
-#     posts = Post.published.all()
-#
-#     # Paginator
-#     object_list = Post.objects.all()
-#     paginator = Paginator(object_list, 3)
-#     page = request.GET.get('page')
-#     # context = {'page':page,'posts': posts}
-#
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         # If page is not an integer deliver the first page
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # If page is out of range deliver last page of results
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 'blog/post/list.html',{'page':page,'posts': posts})
+def post_list(request, tag_slug=None):
+    # posts = Post.published.all()
+    #
+    # # Paginator
+    # object_list = Post.objects.all()
+    # paginator = Paginator(object_list, 3)
+    # page = request.GET.get('page')
+    # tag = None
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page,
+                                                   'posts': posts,
+                                                   'tag': tag})
 
 
 
@@ -58,7 +70,16 @@ def post_detail(request, year, month, day, post):
             comment_form = CommentForm()
     else:
         comment_form = CommentForm()
-    return render(request,'blog/post/detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form})
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    return render(request,'blog/post/detail.html', {'post': post,
+                                                    'comments': comments,
+                                                    'comment_form': comment_form,
+                                                    'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
